959957e1bfa9be8a28f786880c24d64d7564b766
Nabi Ayub <nabiaub908@gmail.com>
2026-01-22 23:13:28 +0500

    feat(database): Add BaseModel in base

---
+++ src/database/models/base.py
@@ -0,0 +1,24 @@
+from datetime import datetime
+
+from sqlalchemy import Integer, TIMESTAMP
+from sqlalchemy.ext.asyncio import AsyncAttrs
+from sqlalchemy.orm import Mapped, mapped_column, DeclarativeBase
+from sqlalchemy.sql import func
+
+class Base(AsyncAttrs, DeclarativeBase):
+    __abstract__ = True
+
+    id: Mapped[int] = mapped_column(Integer, primary_key=True, autoincrement=True)
+
+    created_at: Mapped[datetime] = mapped_column(
+        TIMESTAMP, server_default=func.now()
+    )
+    updated_at: Mapped[datetime] = mapped_column(
+        TIMESTAMP, server_default=func.now(), onupdate=func.now()
+    )
+
+    @classmethod
+    @property
+    def __tablename__(cls) -> str:
+        return cls.__name__.lower() + 's'
+
