418138c867265bce3ed96110c0999b09befa1706
Nabi Ayub <nabiaub908@gmail.com>
2026-01-26 23:44:22 +0500

    refactor(buttons): change cancel button from inline to reply

--- src/bot/users/handlers/profile.py
+++ src/bot/users/handlers/profile.py
@@ -1,36 +1,25 @@
 
-
 from aiogram import Router, F
 from aiogram.filters import StateFilter
 from aiogram.fsm.context import FSMContext
 from aiogram.types import Message, CallbackQuery
 from sqlalchemy.ext.asyncio import AsyncSession
 
-from src.bot.users.handlers.start import start_bot
 from src.bot.users.states import UserNameState
 from src.services.repositories.users import UserRepository
 
 router = Router()
 
-@router.callback_query(UserNameState.NAME, F.data == 'skip_name')
-async def skip_name(callback_query: CallbackQuery,
+@router.message(UserNameState.NAME, F.text == 'Skip')
+async def skip_name(message: Message,
                     state: FSMContext,
                     session_with_commit: AsyncSession) -> None:
-    print('skipped user')
-    user_repo = UserRepository(session_with_commit)
-    await user_repo.set_name(callback_query.from_user.id)
 
-    await callback_query.answer()
+    user_repo = UserRepository(session_with_commit)
+    await user_repo.set_name(message.from_user.id)
 
     await state.clear()
-    await callback_query.message.delete()
-    await callback_query.message.answer("Ok, I won’t ask again. Use /setname anytime.")
+    await message.reply("Ok, I won’t ask again. Use /setname anytime.")
 
-
-@router.callback_query(UserNameState.NAME, F.data == 'skip_name')
-async def skip_name(callback: CallbackQuery,
-                    state: FSMContext,
-                    session_with_commit: AsyncSession) -> None:
-    ...
 
 
--- src/bot/users/keyboards.py
+++ src/bot/users/keyboards.py
@@ -1,15 +1,14 @@
 from typing import List
-from aiogram.types import InlineKeyboardMarkup, ReplyKeyboardMarkup, InlineKeyboardButton
-from aiogram.utils.keyboard import InlineKeyboardBuilder, ReplyKeyboardBuilder
+from aiogram.types import InlineKeyboardMarkup, ReplyKeyboardMarkup
+from aiogram.utils.keyboard import ReplyKeyboardBuilder
 
 
-def cancel_name_kb() -> InlineKeyboardMarkup:
-    keyboard = InlineKeyboardBuilder()
-    keyboard.button(text='Skip', callback_data='skip_name')
+def cancel_name_kb() -> ReplyKeyboardMarkup:
+    keyboard = ReplyKeyboardBuilder()
+    keyboard.button(text='Skip')
 
     keyboard.adjust(1)  # 1 button per row (important for size)
 
     return keyboard.as_markup(
         resize_keyboard=True,  # makes buttons compact
-        one_time_keyboard=True  # optional: hides after press
     )
