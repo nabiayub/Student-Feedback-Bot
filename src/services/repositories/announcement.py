901656447df98d5f48c878e99007273dd2a88f43
Nabi Ayub <nabiaub908@gmail.com>
2026-02-24 16:38:04 +0500

    feat(): created Announcement Repository

---
+++ src/services/repositories/announcement.py
@@ -0,0 +1,25 @@
+from sqlite3 import IntegrityError
+
+from sqlalchemy import select, ScalarResult
+from sqlalchemy.dialects.postgresql import insert
+from sqlalchemy.ext.asyncio import AsyncSession
+
+
+from src.database.models import Announcement
+from src.schemas.announcements import AnnouncementCreate
+
+
+class AnnouncementRepo():
+    def __init__(self, session: AsyncSession) -> None:
+        self.__session = session
+
+    async def create_announcement(self, announcement: AnnouncementCreate):
+        db_announcement = Announcement(**announcement.model_dump())
+        self.__session.add(db_announcement)
+
+
+        try:
+            await self.__session.flush()
+
+        except IntegrityError:
+            await self.__session.rollback()
