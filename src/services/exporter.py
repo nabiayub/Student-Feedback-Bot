e1cd8355ca31c1172687a5e0c44509c4f6a51d99
Nabi Ayub <nabiaub908@gmail.com>
2026-04-09 23:15:24 +0500

    feat: added cvs and excel exporter

---
+++ src/services/exporter.py
@@ -0,0 +1,38 @@
+import pandas as pd
+from src.database.models import Message
+
+class ExportService:
+    @staticmethod
+    def _prepare_data(messages: list[Message]) -> list[dict]:
+        """
+        Helper to convert Message objects into a list of dictionaries for Pandas.
+        """
+        return [
+            {
+                'Content': msg.content,
+                'Category': msg.category.title if msg.category else 'N/A',
+                'Created At': msg.created_at.strftime('%d-%m-%Y %H:%M')
+            }
+            for msg in messages
+        ]
+
+    @staticmethod
+    def generate_csv(messages: list[Message], file_path: str) -> str:
+        """
+        Generates a CSV file from a list of messages.
+        """
+        data = ExportService._prepare_data(messages)
+        df = pd.DataFrame(data)
+        df.to_csv(file_path, index=False, encoding='utf-8-sig')
+        return file_path
+
+    @staticmethod
+    def generate_excel(messages: list[Message], file_path: str) -> str:
+        """
+        Generates an Excel (.xlsx) file from a list of messages.
+        """
+        data = ExportService._prepare_data(messages)
+        df = pd.DataFrame(data)
+        # Note: requires openpyxl to be installed
+        df.to_excel(file_path, index=False, engine='openpyxl')
+        return file_path
